0e8e2550f3552f854627c267d008a6afeccb8d1c
MuhammadAffan623 <affanmuhammad623@gmail.com>
2025-07-13 12:23:43 +0500

    adjusted add project width similar to project card

--- src/components/AddProjectCard.jsx
+++ src/components/AddProjectCard.jsx
@@ -7,7 +7,7 @@ export default function AddButtonCard() {
   return (
     <div
       id="create-btn"
-      className="w-40 h-40 bg-gray-300 rounded-md flex items-center justify-center cursor-pointer"
+      className="w-full max-w-xs h-40 bg-gray-300 rounded-xl flex items-center justify-center cursor-pointer"
       onClick={() => navigate("/add")}
     >
       <div className="w-16 h-16 bg-white rounded-full flex items-center justify-center">
